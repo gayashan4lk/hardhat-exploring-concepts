ca5b80f9c8ea8cb34f732c6d5023ac6a7612595f
gayashan4lk <gayashan4lk@gmail.com>
2023-01-07 13:26:30 +0530

    add deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require('@nomicfoundation/hardhat-toolbox');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: '0.8.17',
+
+	networks: {
+		hardHat: {
+			url: 'http://127.0.0.1:8545/',
+		},
+	},
 };
