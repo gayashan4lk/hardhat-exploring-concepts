3fd16603473a726c75fd777921ffbcf38a537439
gayashan4lk <gayashan4lk@gmail.com>
2023-01-07 14:47:06 +0530

    improve test script with console logs

--- test/Token.js
+++ test/Token.js
@@ -4,12 +4,20 @@ describe('Token contract', function () {
 	it('Deployment should assign the total supply of tokens to the owner', async function () {
 		this.timeout(60000);
 		const [owner] = await ethers.getSigners();
+		console.log('== Owner ==');
+		console.log(owner);
 
 		const Token = await ethers.getContractFactory('Token');
+		console.log('== Token ==');
+		console.log(Token);
 
 		const hardhatToken = await Token.deploy();
+		console.log('== HardHat Token ==');
+		console.log(hardhatToken);
 
 		const ownerBalance = await hardhatToken.balanceOf(owner.address);
+		console.log('== Owner Balance ==');
+		console.log(ownerBalance);
 
 		expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
 	});
